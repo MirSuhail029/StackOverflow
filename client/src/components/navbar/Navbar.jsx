7b501e2edc46ca14b53424bc276693ff37b43b0a
Suhail <mirsuhail029@gmail.com>
2022-11-24 00:16:26 +0530

    Completed the side bars

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -8,7 +8,7 @@ import './Navbar.css'
 const Navbar=()=>{
     let user=null;
     return(
-        <nav>
+        <nav className='main-nav'>
             <div className='navbar'>
                 <Link to='/' className='nav-item nav-btn'>
                     <img src={logo} alt='logo' width='150px' />
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import '../../App.css';
+import LeftSidebar from '../../components/leftSidebar/LeftSidebar';
+import RightSidebar from '../../components/rightSidebar/RightSidebar';
+import HomeMainbar from '../../components/homeMainbar/HomeMainbar';
 
 const Home = () => {
     return (
-        <div>
+        <div className='home-container-1'>
+            <LeftSidebar />
+            <div className='home-container-2'>
+                <HomeMainbar />
+                <RightSidebar />
+            </div>
 
         </div>
     )
