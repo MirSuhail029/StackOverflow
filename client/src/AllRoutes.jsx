62e6758cb1c660e5b8458b88e175cc11889ca0f7
Suhail <mirsuhail029@gmail.com>
2022-12-03 19:55:29 +0530

    Frontend Completed
    Completed the frontend coding for stackOverflow Clone

--- client/src/AllRoutes.jsx
+++ client/src/AllRoutes.jsx
@@ -3,6 +3,8 @@ import {Routes, Route} from 'react-router-dom';
 import Home from './pages/home/Home';
 import Auth from './pages/auth/Auth';
 import Questions from './pages/questions/Questions';
+import AskQuestion from './pages/askquestion/AskQuestion';
+import DisplayQuestion from './pages/questions/DisplayQuestion';
 
 const AllRoutes = () => {
     return (
@@ -10,6 +12,8 @@ const AllRoutes = () => {
             <Route exact path='/' element={<Home />} />
             <Route exact path='/Auth' element={<Auth />} />
             <Route exact path='/Questions' element={<Questions />} />
+            <Route exact path='/AskQuestion' element={<AskQuestion />} />
+            <Route exact path='/Questions/:id' element={<DisplayQuestion />} />
         </Routes>
     )
 }
